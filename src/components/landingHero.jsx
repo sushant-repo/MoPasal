3d716ab48306f291a03eb53834f53ec57e401675
Sushant Thapa <sus.hant@outlook.com>
2021-08-24 10:55:15 +1000

    first commit with finalized color pallete

---
+++ src/components/landingHero.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingHero = () => {
+  return <section className="grid landing-hero"></section>;
+};
+
+export default LandingHero;
---
+++ src/pages/landing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/common/navbar";
+import LandingHero from "../components/landingHero";
+
+const Landing = () => {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <LandingHero />
+    </React.Fragment>
+  );
+};
+
+export default Landing;
